d510a8feacf797bcfb833614f75c16b29f4aedf0
GabrielKishida <gabriel.kishida@usp.br>
2021-01-21 16:24:39 -0300

    :zap: Add Numpy library to Gaussian Elimination algorithm

--- SistemasLineares/npElimGauss.py
+++ SistemasLineares/npElimGauss.py
@@ -3,33 +3,21 @@
 #Date: January/2021
 
 #Description : Algorithm that applies the method of Gaussian Elimination to linear systems.
+#This version utilizes the numpy library to make things easier (and faster).
 
-#Functionality: adds two vectors, each element one by one and returns the sum.
-def vectorsum(a,b):
-    c = []
-    for i in range(len(a)):
-        c.append(a[i] + b[i])
-    return c
-
-#Functionality: multiplies a vector by a constant "mult", and returns the new vector.
-def vectormult(mult,vector):
-    c = []
-    for i in range(len(vector)):
-        c.append(vector[i]*mult)
-    return c
-
-#Functionality: divides a vector by a constant "div", and returns the new vector.
-def vectordiv(div,vector):
-    c = []
-    for i in range(len(vector)):
-        c.append(vector[i]/div)
-    return c
+import numpy as np
 
 #Functionality: this function switches line1 and line2 in a matrix.
-def switchlines(matrix,line1,line2):
+def switchLines(matrix,line1,line2):
     auxvector = matrix[line1]
     matrix[line1] = matrix[line2]
     matrix[line2] = auxvector
+
+#Functionality: this function subtracts a linear proportion (equal to coef) of a maintaned line
+#from an eliminated line, making one element (matrix[lineEliminated][lineMaintaned]) equal to 0.
+def lineElimination(matrix,lineMaintained,lineEliminated):
+    coef = -(matrix[lineEliminated][lineMaintained]/matrix[lineMaintained][lineMaintained])
+    matrix[lineEliminated] = matrix[lineEliminated] + matrix[lineMaintained]*coef
 
 #Functionality: applies the gaussian elimination method, returning the solved matrix.
 #Input: the input must be a matrix[n][n+1], where n is any integer > 0.
@@ -42,32 +30,28 @@ def elimgauss(matrix):
             allZeroes = True
             for k in range(i+1,n):
                 if(matrix[i][k] != 0):
-                    switchlines(matrix,i,k)
+                    switchLines(matrix,i,k)
+                    print("switchline")
+                    print(matrix)
                     allZeroes = False
                     break
             if (allZeroes): return "System not solvable"
-         #Firstly, this method "eliminates" the values in the lower triangle of the matrix.
+        #Firstly, this method "eliminates" the values in the lower triangle of the matrix.
         for j in range(i+1,n):
-            if (matrix[i][i] == 0 ): return "System not solvable"
-            coef = -(matrix[j][i]/matrix[i][i])
-            auxvector = vectormult(coef,matrix[i])
-            matrix[j] = vectorsum(matrix[j],auxvector)
-            print(matrix)
+            lineElimination(matrix,i,j)
     if (matrix[n-1][n-1] == 0): return "System not solvable"
-    matrix[n-1] = vectordiv(matrix[n-1][n-1],matrix[n-1])
+    matrix[n-1] = matrix[n-1]/matrix[n-1][n-1]
     #Then, this method works bottom-up, eliminating the values in the upper triangle of the matrix.
     for i in range(n-1,-1,-1):
         for j in range(i-1,-1,-1):
-            coef = -(matrix[j][i]/matrix[i][i])
-            auxvector = vectormult(coef,matrix[i])
-            matrix[j] = vectorsum(matrix[j],auxvector)
+            lineElimination(matrix,i,j)
     #Finally, this method must divide each row by its element on the main diagonal, constructing the
     #identity matrix.
     for i in range(n):
-        matrix[i] = vectordiv(matrix[i][i],matrix[i])
+        matrix[i] = matrix[i]/matrix[i][i]
     return matrix
 
 #Example test to which the algorithm runs.
-matrix = [[1,1,2,0,1],[2,-1,0,1,-2],[1,-1,-1,-2,4],[2,-1,2,-1,0]]
+matrix = np.array([[1.0,1.0,2.0,0.0,1.0],[2.0,-1.0,0.0,1.0,-2.0],[1.0,-1.0,-1.0,-2.0,4.0],[2.0,-1.0,2.0,-1.0, 0.0]])
 print(elimgauss(matrix))
 print("Expected values: x1 = 1, x2 = 2, x3 = -1, x4 = -2")
