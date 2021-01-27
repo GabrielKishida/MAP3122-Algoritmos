bc8e1cda9691cd8e2c1566487e5a7938ea76be21
GabrielKishida <gabriel.kishida@usp.br>
2021-01-27 20:20:12 -0300

    :sparkles: Add pivotal condensation

--- SistemasLineares/npElimGauss.py
+++ SistemasLineares/npElimGauss.py
@@ -9,7 +9,7 @@ import numpy as np
 
 #Functionality: this function switches line1 and line2 in a matrix.
 def switchLines(matrix,line1,line2):
-    auxvector = matrix[line1]
+    auxvector = np.array(matrix[line1])
     matrix[line1] = matrix[line2]
     matrix[line2] = auxvector
 
@@ -51,7 +51,34 @@ def elimgauss(matrix):
         matrix[i] = matrix[i]/matrix[i][i]
     return matrix
 
+def pivotalElimGauss(matrix):
+    n = len(matrix)
+    for i in range(n):
+        #Before we start the method, we need to apply pivotal condensation
+        biggestPivot = i
+        for k in range(i,n) :
+            if (abs(matrix[biggestPivot][i]) < abs(matrix[k][i])) :
+                biggestPivot = k
+        if (biggestPivot != i) :
+            switchLines(matrix,i,biggestPivot)
+        #Firstly, this method "eliminates" the values in the lower triangle of the matrix.
+        for j in range(i+1,n):
+            lineElimination(matrix,i,j)
+    if (matrix[n-1][n-1] == 0): return "System not solvable"
+    matrix[n-1] = matrix[n-1]/matrix[n-1][n-1]
+    #Then, this method works bottom-up, eliminating the values in the upper triangle of the matrix.
+    for i in range(n-1,-1,-1):
+        for j in range(i-1,-1,-1):
+            lineElimination(matrix,i,j)
+    #Finally, this method must divide each row by its element on the main diagonal, constructing the
+    #identity matrix.
+    for i in range(n):
+        matrix[i] = matrix[i]/matrix[i][i]
+    return matrix
+
+
 #Example test to which the algorithm runs.
 matrix = np.array([[1.0,1.0,2.0,0.0,1.0],[2.0,-1.0,0.0,1.0,-2.0],[1.0,-1.0,-1.0,-2.0,4.0],[2.0,-1.0,2.0,-1.0, 0.0]])
-print(elimgauss(matrix))
+print(matrix)
+print(pivotalElimGauss(matrix))
 print("Expected values: x1 = 1, x2 = 2, x3 = -1, x4 = -2")
